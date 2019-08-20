3825af3d42ef65d65d5fe04f87c8f32178af7763
Ronnie Geraghty <ronniegerag@gmail.com>
2019-08-20 11:46:37 -0400

    test with home in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ class App extends Component {
       <React.Fragment>
         <NavBar />
         <Switch>
+          <Home />
           <Redirect exact path="/" to="/Home" />
           <Route path="/Home" component={Home} />
           <Route path="/Ronnie" component={Ronnie} />
