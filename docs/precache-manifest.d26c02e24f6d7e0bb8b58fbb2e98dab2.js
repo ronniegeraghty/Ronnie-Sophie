61ed5667dc75d83c8d5c5bca1cbbfb92074ca812
Ronnie Geraghty <ronniegerag@gmail.com>
2019-08-20 10:44:49 -0400

    pushing docs folder test 001

---
+++ docs/precache-manifest.d26c02e24f6d7e0bb8b58fbb2e98dab2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9de20a5d2578aa697d13c764b28481b8",
+    "url": "/Ronnie-Sophie/index.html"
+  },
+  {
+    "revision": "3427650e1b9ca787a120",
+    "url": "/Ronnie-Sophie/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b114a08a745080c5d515",
+    "url": "/Ronnie-Sophie/static/css/main.674d243f.chunk.css"
+  },
+  {
+    "revision": "3427650e1b9ca787a120",
+    "url": "/Ronnie-Sophie/static/js/2.f920a85c.chunk.js"
+  },
+  {
+    "revision": "b114a08a745080c5d515",
+    "url": "/Ronnie-Sophie/static/js/main.6585ce3a.chunk.js"
+  },
+  {
+    "revision": "a8fc842f675a4299e2b8",
+    "url": "/Ronnie-Sophie/static/js/runtime~main.034971b6.js"
+  }
+]);
