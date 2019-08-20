334c0d821500ec0c1820e0442bc901ce9c0371bb
Ronnie Geraghty <ronniegerag@gmail.com>
2019-08-20 10:31:04 -0400

    added docs folder

---
+++ docs/precache-manifest.e3bca7ee69e2becc0c867dc72895a97d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb8d56b8274a12ea486e7989c21dce8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3427650e1b9ca787a120",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e473ec066336aaa690c2",
+    "url": "/static/css/main.674d243f.chunk.css"
+  },
+  {
+    "revision": "3427650e1b9ca787a120",
+    "url": "/static/js/2.f920a85c.chunk.js"
+  },
+  {
+    "revision": "e473ec066336aaa690c2",
+    "url": "/static/js/main.fc9a1c0c.chunk.js"
+  },
+  {
+    "revision": "eea69626c05882b096c1",
+    "url": "/static/js/runtime~main.20c5a6de.js"
+  }
+]);
