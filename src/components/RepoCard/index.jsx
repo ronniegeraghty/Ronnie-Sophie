4ddfac6e5c19f852994cc18743ceccdf1370f620
Ronnie Geraghty <ronniegerag@gmail.com>
2019-08-23 13:59:35 -0400

    test

--- src/components/RepoCard/index.jsx
+++ src/components/RepoCard/index.jsx
@@ -11,10 +11,7 @@ class RepoCard extends Component {
             <Card.Subtitle className="mb-2 text-muted">
               Repo Description
             </Card.Subtitle>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
+            <Card.Text>Repo Description</Card.Text>
             <Card.Link href="#">Card Link</Card.Link>
             <Card.Link href="#">Another Link</Card.Link>
           </Card.Body>
